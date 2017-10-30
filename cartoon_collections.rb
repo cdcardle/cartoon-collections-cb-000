c865260b1caef82e9639a4e56dd8028bb68b96ef
Chris Cardle <cdcardle@gmail.com>
2017-10-30 16:53:54 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,9 +1,9 @@
-def roll_call_dwarves(array)
-  array.each_with_index {|n, i| puts "#{i+1}. #{n}"}
+def roll_call_dwarves(a)
+  a.each_with_index {|n, i| puts "#{i+1}. #{n}"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(a)
+  a.map {|n| "#{n.upcase}!"}
 end
 
 def long_planeteer_calls# code an argument here
