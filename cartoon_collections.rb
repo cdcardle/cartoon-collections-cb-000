a483e8a14788c7950cc35af52daa4498b0010d8a
Chris Cardle <cdcardle@gmail.com>
2017-10-30 16:46:56 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,5 @@
 def roll_call_dwarves(array)
-  array.each_with_index {|name, index| "#{index}. #{name}"}
+  array.each_with_index {|name, index| puts "#{index}. #{name}"}
 end
 
 def summon_captain_planet# code an argument here
@@ -14,3 +14,4 @@ def find_the_cheese# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
+
